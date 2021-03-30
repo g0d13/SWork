d9092904c09b6212fd463764f3aae747f448fd9d
Diego <g0d13@outlook.com>
2021-03-30 15:59:36 -0600

    update app file with layout and routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { RecoilRoot } from "recoil";
+import Layout from "./layout/Layout";
+import { Router } from "@reach/router";
+import Logs from "./pages/Logs";
+import Users from "./pages/Users";
+import Login from "./pages/Login";
+import Request from "./pages/Log/Log";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <RecoilRoot>
+        <Router>
+          <Layout path="/" redirect="/home">
+            <Logs path="/home" />
+            <Request path="/request" />
+            <Users path="/users" />
+          </Layout>
+          <Login path="/login" />
+        </Router>
+      </RecoilRoot>
+    </>
   );
 }
 
