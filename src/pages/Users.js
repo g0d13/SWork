ad8b6a212ce24c3c5cdfb6d2d39a61c10dfb1efa
Diego <g0d13@outlook.com>
2021-03-31 16:57:43 -0600

    feat: add user page

--- src/pages/Users.js
+++ src/pages/Users.js
@@ -1,12 +1,42 @@
-import { List, ListItem, ListItemIcon, ListItemText } from "@material-ui/core";
-import InboxIcon from "@material-ui/icons/Inbox";
+import {
+  Avatar,
+  List,
+  ListItem,
+  ListItemAvatar,
+  ListItemText,
+} from "@material-ui/core";
+import { Person } from "@material-ui/icons";
+import { useNavigate } from "@reach/router";
+import { useSelector, useDispatch } from "react-redux";
+import { useEffect } from "react";
+import { getUsers } from "../store/usersSlice";
 
 const Users = () => {
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { userList } = useSelector((state) => state.users);
+  useEffect(() => {
+    dispatch(getUsers());
+  }, [dispatch]);
+
   return (
     <List>
-      <ListItem button>
-        <ListItemText primary="Usuario 1" />
-      </ListItem>
+      {userList.map((el, index) => {
+        return (
+          <ListItem
+            button
+            onClick={() => navigate(`/users/${el.userId}`)}
+            key={index}
+          >
+            <ListItemAvatar>
+              <Avatar>
+                <Person />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText primary={el.firstName} secondary={el.role} />
+          </ListItem>
+        );
+      })}
     </List>
   );
 };
