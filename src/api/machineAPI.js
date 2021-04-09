fcfab6833ff3d350a61fa7c615a976a8c2e8ff4c
Diego <g0d13@outlook.com>
2021-04-08 22:25:57 -0500

    remove log api and machine api

--- src/api/machineAPI.js
+++
@@ -1,9 +0,0 @@
-import httpClient from "./httpClient";
-
-async function getMachines() {
-  const { data } = await httpClient.get("/api/machine");
-  return data;
-}
-
-// eslint-disable-next-line import/no-anonymous-default-export
-export default { getMachines };
