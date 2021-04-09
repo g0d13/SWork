6789a566b178a82ef2232ddfb68a46172c91f6e3
Diego <g0d13@outlook.com>
2021-04-08 22:24:53 -0500

    feat: add auth slice

---
+++ src/store/authSlice.js
@@ -0,0 +1,65 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import httpClient from "../api/httpClient";
+
+const module_name = "auth";
+
+const initialState = {
+  jwt: "",
+  status: "no_loggin",
+  user: {
+    name: "",
+    email: "",
+    role: "",
+  },
+};
+
+export const doLogin = createAsyncThunk(
+  `${module_name}/doLogin`,
+  async (data) => {
+    const response = await httpClient.post("/api/Auth/Login", data);
+    const token = response.data.token;
+    httpClient.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+    return response;
+  }
+);
+
+export const refreshToken = createAsyncThunk(
+  `${module_name}/refreshToken`,
+  async () => {
+    const response = httpClient.post(
+      "/api/Auth/RefreshToken",
+      {},
+      { withCredentials: true }
+    );
+    httpClient.defaults.headers.common[
+      "Authorization"
+    ] = `Bearer ${response.data.token}`;
+    return response.data;
+  }
+);
+
+const authSlice = createSlice({
+  name: module_name,
+  initialState: initialState,
+  reducers: {
+    isLoggedUser: (state) => !!!state.jwt,
+  },
+  extraReducers: {
+    [doLogin.fulfilled]: (state, { meta, payload }) => {
+      const { token, ...user } = payload.data;
+      state.jwt = token;
+      state.user = user;
+      state.status = "ok";
+    },
+    [doLogin.pending]: (state, { meta, payload }) => {
+      state.status = "pending";
+    },
+    [doLogin.rejected]: (state, { meta, payload }) => {
+      state.status = "usuario o contrasenia invalidos";
+    },
+  },
+});
+
+export const { actions } = authSlice;
+
+export default authSlice.reducer;
