2994a7bf0ecc922e43926ef8ac4992774dbb98ba
Diego <g0d13@outlook.com>
2021-04-11 12:16:35 -0500

    fix: remove infinite loop caused by dependencies

--- src/hooks/useStateFetch.js
+++ src/hooks/useStateFetch.js
@@ -3,9 +3,12 @@ import { useDispatch } from "react-redux";
 
 const useStateFetch = (selector, reducer) => {
   const dispatch = useDispatch();
+
   useEffect(() => {
-    if (selector.length === 0) dispatch(reducer);
-  }, [dispatch, reducer, selector]);
+    if (selector.length === 0) {
+      dispatch(reducer);
+    }
+  }, []);
 };
 
 export default useStateFetch;
