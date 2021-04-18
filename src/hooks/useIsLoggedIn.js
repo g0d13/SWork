5a45d74e4dfd9106c77e0aaf55b5b6a39938534e
Diego <g0d13@outlook.com>
2021-04-18 16:25:39 -0500

    useIsLoggedIn.js now executes the ok and cancel functions when not is defined

--- src/hooks/useIsLoggedIn.js
+++ src/hooks/useIsLoggedIn.js
@@ -1,15 +1,18 @@
 import { useEffect } from "react";
 import { useSelector } from "react-redux";
 
-const useIsLoggedIn = (ok = () => {}, cancel = () => {}) => {
-  const isLoggedUser = useSelector((state) => !!state.auth.jwt);
+const useIsLoggedIn = ({ ok, cancel }) => {
+  const user = JSON.parse(localStorage.getItem("user"));
+  const token = useSelector((state) => !!state.auth.jwt);
+  const isLoggedUser = user?.token ?? token;
+
   useEffect(() => {
-    if (isLoggedUser === true) {
-      ok();
+    if (isLoggedUser) {
+      ok && ok();
     } else {
-      cancel();
+      cancel && cancel();
     }
-  }, [cancel, isLoggedUser, ok]);
+  }, [isLoggedUser, ok, cancel]);
 };
 
 export default useIsLoggedIn;
