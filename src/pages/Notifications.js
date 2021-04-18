912e90e10a35de0201f71ebd20af85d14c6541ca
Diego <g0d13@outlook.com>
2021-04-18 16:27:31 -0500

    Notifications detect when show quantity

--- src/pages/Notifications.js
+++ src/pages/Notifications.js
@@ -8,31 +8,66 @@ import {
 import { Notifications as Notify } from "@material-ui/icons";
 import { useNavigate } from "@reach/router";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { modifyUiTitle } from "../store/slices/ui";
+import { fetchNotifications, selectAll } from "../store/slices/notifications";
+import useStateFetch from "../hooks/useStateFetch";
 
-const Notifications = () => {
+const Notifications = ({ quantity }) => {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  const navigateTo = (id) => {
-    navigate(id);
-  };
+  const notificationsList = useSelector(selectAll);
+  useStateFetch(notificationsList, fetchNotifications());
 
   useEffect(() => {
-    dispatch(modifyUiTitle("Notificationes"));
-  }, [dispatch]);
+    !quantity && dispatch(modifyUiTitle("Notificationes"));
+  }, [dispatch, notificationsList, quantity]);
+
+  const getNotificationsList = () => {
+    if (quantity === undefined || quantity > notificationsList.length) {
+      return notificationsList;
+    }
+    return notificationsList.splice(0, quantity);
+  };
+  const repairItem = (request) => (
+    <ListItem
+      button
+      onClick={() => navigate(`/notify/repair/${request.id}`)}
+      key={request.id}
+    >
+      <ListItemAvatar>
+        <Avatar>
+          <Notify />
+        </Avatar>
+      </ListItemAvatar>
+      <ListItemText primary="Repair" secondary="Realizado" />
+    </ListItem>
+  );
+
+  const requestItem = (request) => (
+    <ListItem
+      button
+      onClick={() => navigate(`/notify/repair/${request.id}`)}
+      key={request.id}
+    >
+      <ListItemAvatar>
+        <Avatar>
+          <Notify />
+        </Avatar>
+      </ListItemAvatar>
+      <ListItemText
+        primary={request.problemCode}
+        secondary={request.description}
+      />
+    </ListItem>
+  );
 
   return (
     <List>
-      <ListItem button onClick={() => navigateTo("/notify/id")}>
-        <ListItemAvatar>
-          <Avatar>
-            <Notify />
-          </Avatar>
-        </ListItemAvatar>
-        <ListItemText primary="Trabajo 1" secondary="Realizado" />
-      </ListItem>
+      {getNotificationsList().map((el) =>
+        el.isFixed ? repairItem(el) : requestItem(el)
+      )}
     </List>
   );
 };
