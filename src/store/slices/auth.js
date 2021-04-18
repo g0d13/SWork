860474b0a62800311649fe4078c87d9c9ac2a5b0
Diego <g0d13@outlook.com>
2021-04-18 16:17:15 -0500

    feat(auth): save jwt token to localstorage

--- src/store/slices/auth.js
+++ src/store/slices/auth.js
@@ -1,15 +1,18 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import httpClient from "../../api/httpClient";
+import { connection } from "../helpers/signalr";
 
 const module_name = "auth";
 
 const initialState = {
   jwt: "",
   status: "no_logging",
+  message: "",
   user: {
     firstName: "",
     email: "",
     role: "",
+    id: "",
   },
 };
 
@@ -19,7 +22,7 @@ export const doLogin = createAsyncThunk(
     const response = await httpClient.post("/api/Auth/Login", data);
     const token = response.data.token;
     httpClient.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-
+    connection.invoke("OnConnectionReady", response.data.id).catch(() => {});
     return response;
   }
 );
@@ -43,24 +46,30 @@ const auth = createSlice({
   name: module_name,
   initialState: initialState,
   reducers: {
-    isLoggedUser: (state) => !!!state.jwt,
+    getUserData: (state) => {
+      const { jwt, ...user } = JSON.parse(window.localStorage.getItem("user"));
+      state.jwt = jwt;
+      state.user = user;
+    },
   },
   extraReducers: {
-    [doLogin.fulfilled]: (state, { meta, payload }) => {
+    [doLogin.fulfilled]: (state, { payload }) => {
       let { token, ...user } = payload.data;
       state.jwt = token;
       state.user = user;
-      state.status = "ok";
+      state.message = "Inicio de sesion correcto";
+      window.localStorage.setItem("user", JSON.stringify(payload.data));
     },
-    [doLogin.pending]: (state, { meta, payload }) => {
-      state.status = "pending";
+    [doLogin.pending]: (state) => {
+      state.message = "cargando...";
     },
-    [doLogin.rejected]: (state, { meta, payload }) => {
-      state.status = "usuario o contrasenia invalidos";
+    [doLogin.rejected]: (state, action) => {
+      state.message = action.error.message;
     },
   },
 });
 
 export const { actions } = auth;
+export const { getUserData } = auth.actions;
 
 export default auth.reducer;
