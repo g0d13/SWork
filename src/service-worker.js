4de44dcdb87d0894460b4015a74a380ea20d9b45
Diego <g0d13@outlook.com>
2021-04-20 13:10:26 -0500

    feat: service worker push notifications

--- src/service-worker.js
+++ src/service-worker.js
@@ -71,3 +71,23 @@ self.addEventListener("message", (event) => {
 });
 
 // Any other custom service worker logic can go here.
+self.addEventListener("push", function (e) {
+  var options = {
+    body: "This notification was generated from a push!",
+    icon: "icon-512x512.png",
+    vibrate: [100, 50, 100],
+    data: {
+      dateOfArrival: Date.now(),
+      primaryKey: "2",
+    },
+    actions: [
+      {
+        action: "explore",
+        title: "Explore this new world",
+        icon: "images/checkmark.png",
+      },
+      { action: "close", title: "Close", icon: "images/xmark.png" },
+    ],
+  };
+  e.waitUntil(self.registration.showNotification("Hello world!", options));
+});
--- src/serviceWorkerRegistration.js
+++ src/serviceWorkerRegistration.js
@@ -54,10 +54,49 @@ export function register(config) {
   }
 }
 
+function urlBase64ToUint8Array(base64String) {
+  const padding = "=".repeat((4 - (base64String.length % 4)) % 4);
+  const base64 = (base64String + padding).replace(/-/g, "+").replace(/_/g, "/");
+
+  const rawData = window.atob(base64);
+  const outputArray = new Uint8Array(rawData.length);
+
+  for (let i = 0; i < rawData.length; ++i) {
+    outputArray[i] = rawData.charCodeAt(i);
+  }
+  return outputArray;
+}
+
+async function subscribeUser() {
+  const { VAPID_PUBLIC_KEY } = await fetch(
+    "http://localhost:3030/api/notify/preload/id"
+  ).then((response) => response.json());
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.ready.then(function (reg) {
+      reg.pushManager
+        .subscribe({
+          userVisibleOnly: true,
+          applicationServerKey: urlBase64ToUint8Array(VAPID_PUBLIC_KEY),
+        })
+        .then(function (sub) {
+          console.log("Endpoint URL: ", sub.endpoint);
+        })
+        .catch(function (e) {
+          if (Notification.permission === "denied") {
+            console.warn("Permission for notifications was denied");
+          } else {
+            console.error("Unable to subscribe to push", e);
+          }
+        });
+    });
+  }
+}
+
 function registerValidSW(swUrl, config) {
   navigator.serviceWorker
     .register(swUrl)
     .then((registration) => {
+      console.log("Service Worker Registered!", registration);
       registration.onupdatefound = () => {
         const installingWorker = registration.installing;
         if (installingWorker == null) {
@@ -92,6 +131,21 @@ function registerValidSW(swUrl, config) {
           }
         };
       };
+      registration.pushManager
+        .getSubscription()
+        .then(function (sub) {
+          if (sub === null) {
+            // Update UI to ask user to register for Push
+            console.log("Not subscribed to push service!");
+            subscribeUser();
+          } else {
+            // We have a subscription, update the database
+            console.log("Subscription object: ", sub);
+          }
+        })
+        .catch(function (err) {
+          console.log("Service Worker registration failed: ", err);
+        });
     })
     .catch((error) => {
       console.error("Error during service worker registration:", error);
