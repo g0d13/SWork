4dcb91065eb30514b4f2757d4cc125eba5282ec6
Juan Diego Parra Mendez <g0d13@outlook.com>
2021-04-26 10:16:40 -0500

    feat(History): add components to show the admin history

---
+++ src/pages/History/HistoryItem.js
@@ -0,0 +1,80 @@
+import { Box, Typography } from "@material-ui/core";
+import React, { useEffect, useState } from "react";
+import { useQuery } from "react-query";
+import { fetchLogById, fetchLogHistory } from "../../api/logsAPI";
+import Loading from "../../components/Loading";
+import {
+  Timeline,
+  TimelineConnector,
+  TimelineContent,
+  TimelineDot,
+  TimelineItem,
+  TimelineOppositeContent,
+  TimelineSeparator,
+} from "@material-ui/lab";
+import { Check, Receipt } from "@material-ui/icons";
+
+import useUiTitle from "../../hooks/useUiTitle";
+
+const GTimeLineItem = ({ text, opposite, icon }) => {
+  return (
+    <TimelineItem>
+      <TimelineOppositeContent>
+        <Typography color="textSecondary">{opposite}</Typography>
+      </TimelineOppositeContent>
+      <TimelineSeparator>
+        {icon ?? <TimelineDot variant="outlined" />}
+        <TimelineConnector />
+      </TimelineSeparator>
+      <TimelineContent>{text}</TimelineContent>
+    </TimelineItem>
+  );
+};
+
+const GenerateRequestTimeline = ({ logId }) => {
+  const logHistory = useQuery(["logHistory", logId], () =>
+    fetchLogHistory(logId)
+  );
+  if (logHistory.isLoading) return <Loading />;
+
+  return logHistory.data.map((history) => (
+    <React.Fragment>
+      <GTimeLineItem
+        text={`Solicitud ${history.description}`}
+        opposite={history.createdAt}
+        icon={<Receipt />}
+      />
+      {history.repair && (
+        <GTimeLineItem
+          text={`Reparada ${history.repair.id}`}
+          opposite={history.repair.createdAt}
+        />
+      )}
+    </React.Fragment>
+  ));
+};
+
+const HistoryItem = ({ id }) => {
+  const [logState, setLogState] = useState({});
+  const logQuery = useQuery(["log", id], () => fetchLogById(id));
+  useUiTitle(logState?.name);
+  useEffect(() => {
+    setLogState(logQuery.data);
+  }, [logQuery.data]);
+
+  if (logQuery.isLoading) return <Loading />;
+
+  return (
+    <Box>
+      <Timeline align="alternate">
+        <GTimeLineItem
+          text={`Bitacora ${logQuery.data.name}`}
+          opposite={logQuery.data.createdAt}
+          icon={<Check />}
+        />
+        <GenerateRequestTimeline logId={id} />
+      </Timeline>
+    </Box>
+  );
+};
+export default HistoryItem;
---
+++ src/pages/History/HistoryList.js
@@ -0,0 +1,47 @@
+import {
+  Card,
+  CardActionArea,
+  CardContent,
+  CardHeader,
+  Typography,
+} from "@material-ui/core";
+import GridView from "../../components/GridView";
+import { useQuery } from "react-query";
+import { fetchDeletedLog } from "../../api/logsAPI";
+import Loading from "../../components/Loading";
+import React from "react";
+import useUiTitle from "../../hooks/useUiTitle";
+import { useNavigate } from "@reach/router";
+
+const LogItemView = ({ log }) => {
+  const navigate = useNavigate();
+  return (
+    <Card onClick={() => {}} variant="outlined">
+      <CardActionArea
+        component="div"
+        onClick={() => navigate(`/history/${log.id}`)}
+      >
+        <CardHeader title={log.name} subheader={`Creado: ${log.createdAt}`} />
+        <CardContent>
+          {log.deletedAt && <Typography>Eliminado: {log.deletedAt}</Typography>}
+        </CardContent>
+      </CardActionArea>
+    </Card>
+  );
+};
+
+const HistoryList = () => {
+  const logQuery = useQuery("logDeleted", fetchDeletedLog);
+
+  useUiTitle("Historial de bitacoras");
+
+  if (logQuery.isLoading) return <Loading />;
+  return (
+    <GridView>
+      {logQuery.data.map((el) => (
+        <LogItemView key={el.id} log={el} />
+      ))}
+    </GridView>
+  );
+};
+export default HistoryList;
